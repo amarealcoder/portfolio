6622c542ecf0872340e1de71daedb8e8e0e20078
amarealcoder <ugorjimiracle06@gmail.com>
2021-11-02 23:25:17 +0000

    navbar component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+export default function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        aria-controls='simple-menu'
+        aria-haspopup='true'
+        onClick={handleClick}
+      >
+        <MenuIcon />
+      </Button>
+      <Menu
+        id='simple-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem component='a' href='/'>
+          Click 
+        </MenuItem>
+        <MenuItem component='a' href='/path'>
+          Click Me
+        </MenuItem>
+        <MenuItem component='a' href='/your-path'>
+          Click Me
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
